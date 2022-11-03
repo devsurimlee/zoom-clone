72e4e49a82b0355f935c3bb4cc23be92162c0a37
devsurimlee <devsurimlee@gmail.com>
2022-11-03 23:04:50 +0900

    2.2 socket.io로 통신하기

--- src/server.js
+++ src/server.js
@@ -16,7 +16,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    console.log(socket);
+    socket.on("enter_room", (msg, callback) => {
+        console.log(msg);
+        setTimeout(() => {
+            callback();
+        }, 3000);
+    });
 });
 
 httpServer.listen(3000, handleListen);
