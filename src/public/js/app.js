de6052a719d0c8fbbc17e705bd092b29bdaf3881
devsurimlee <devsurimlee@gmail.com>
2022-11-04 01:18:38 +0900

    2.6 채팅방 나갈시 퇴장이벤트, 채팅 전송시 다른사람에게 발송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,6 +8,7 @@ room.hidden = true;
 
 let roomName = "";
 
+// 채팅 메세지 생성
 function addMessage(message) {
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
@@ -15,13 +16,27 @@ function addMessage(message) {
     ul.append(li);
 }
 
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${ value }`);
+    });
+    input.value = "";
+}
+
+// 채팅방 입장시 채팅방명 노출
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const title = document.getElementById("title");
-    title.innerText = `Room ${ roomName }`;
+    title.innerText = `Room ${roomName}`;
+    const form = room.querySelector("form");
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
+// 채팅방 입장
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
@@ -35,3 +50,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
     addMessage("Someone joined~");
 });
+
+socket.on("bye", () => {
+    addMessage("Someone left~");
+});
+
+socket.on("new_message", addMessage);
